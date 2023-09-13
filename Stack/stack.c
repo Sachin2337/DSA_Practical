52a4027daf45d4258d5cffa50d45e67761df5983
Shubham-Naik-GU <mca.2334@unigoa.ac.in>
2023-09-13 15:21:09 +0530

    added createstack code

---
+++ Stack/stack.c
@@ -0,0 +1,26 @@
+#include "stack.h"
+#include<stdlib.h>
+
+
+//create stack
+void createStack(st *s){
+    s->top = -1;
+}
+
+int isFull(st* s){
+    if(s->top==SIZE-1){
+        return 1;
+    }
+    else 
+    return 0;
+}
+
+int isEmpty(st *s)
+{
+    if(s->top==-1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
