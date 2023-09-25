b65691560aa6f7823bb22918c7d92b2ace6e9101
Sachin2337 <mca.2337@unigoa.ac.in>
2023-09-25 08:24:41 -0700

    added code

---
+++ TreeTraversal/TreeTraversal.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include "TreeTraversal.h"
+
+// Define the structure for a binary tree node
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+typedef struct TreeNode TreeNode;
+
+// Function to create a new binary tree node
+TreeNode* createNode(int data) {
+    TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to perform a preorder traversal of a binary tree
+void preorderTraversal(TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    printf("%d ", root->data);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+// Function to perform an inorder traversal of a binary tree
+void inorderTraversal(TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    inorderTraversal(root->left);
+    printf("%d ", root->data);
+    inorderTraversal(root->right);
+}
+
+// Function to perform a postorder traversal of a binary tree
+void postorderTraversal(TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    printf("%d ", root->data);
+}
+
+// Function to perform tree traversal algorithms
+void treeTraversal() {
+    // Example tree traversal
+    printf("Tree Traversal Algorithms selected.\n");
+
+    // Create a sample binary tree
+    TreeNode* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Preorder Traversal: ");
+    preorderTraversal(root);
+    printf("\n");
+
+    printf("Inorder Traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    printf("Postorder Traversal: ");
+    postorderTraversal(root);
+    printf("\n");
+
+    // Implement your tree traversal algorithms functionality here
+}
