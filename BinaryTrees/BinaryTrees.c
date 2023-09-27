e869b8fa9528da163c6d3a44ff2055502356ad89
Shubham-Naik <mca.2334@unigoa.ac.in>
2023-09-27 14:26:49 +0530

    added binarytrees.c

--- BinaryTrees/BinaryTrees.c
+++ BinaryTrees/BinaryTrees.c
@@ -1,7 +1,34 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#include <stdio.h>
+#include "BinaryTrees.h"
 
-// Function prototypes for binary trees
-void binaryTrees();
+// Define the structure for a binary tree node
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
 
-#endif
+typedef struct TreeNode TreeNode;
+
+// Function to create a new binary tree node
+TreeNode* createNode(int data) {
+    TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to perform binary tree operations
+void binaryTrees() {
+    // Example binary trees
+    printf("Binary Trees selected.\n");
+
+    // Create a sample binary tree
+    TreeNode* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+}
