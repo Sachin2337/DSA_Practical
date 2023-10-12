736423c8c678dfc55e9197672da3f8cec7589411
sachin59921 <sachinpal59921@gmail.com>
2023-10-12 04:03:27 -0700

    added code to TreeTraversal.h

--- TreeTraversal/TreeTraversal.h
+++ TreeTraversal/TreeTraversal.h
@@ -1,1 +1,9 @@
-//To-do
+#ifndef TREE_TRAVERSAL
+#define TREE_TRAVERSAL
+#include "../BinaryTrees/BinaryTrees.h"
+ 
+void inorderTraversal(TreeNode* root);
+void preorderTraversal(TreeNode* node);
+void postorderTraversal(TreeNode* node); 
+
+#endif
